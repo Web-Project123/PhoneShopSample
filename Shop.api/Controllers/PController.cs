6783c004b7869a3ba0411a5701a93874f2e272ab
Ali <ali81.zakeri81@gmail.com>
2026-02-09 21:03:34 +0330

    Add Validators

---
+++ Shop.api/Controllers/PController.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+
+using Microsoft.AspNetCore.Mvc;
+using PhoneShop.Application.DTOs;
+
+namespace Shop.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create(CreateProductDto dto)
+        {
+            return Ok("محصول ثبت شد");
+        }
+    }
+}
---
+++ Shop.api/Validators/ProductValidator.cs
@@ -0,0 +1,25 @@
+﻿
+
+using FluentValidation;
+using PhoneShop.Application.DTOs;
+
+
+
+namespace Shop.api.Validators
+{
+    public class CreateProductValidator : AbstractValidator<CreateProductDto>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("نام محصول اجباری است")
+                .MinimumLength(3).WithMessage("نام محصول حداقل ۳ کاراکتر باشد");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("قیمت باید بزرگتر از صفر باشد");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("توضیحات نمی‌تواند خالی باشد");
+        }
+    }
+}
